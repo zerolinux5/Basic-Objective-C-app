b77fa430c5b77016b455595c741eb87cf5d488d6
Jesus Magana <maganazero5@gmail.com>
2014-06-22 10:01:36 -0700

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Jesus Magana on 6/22/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
